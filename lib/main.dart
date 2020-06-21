f805d9abe5a625fcc53d65c8c12bf3ed4d1c225f
mutohiroshi <mutohiroshi@team-lab.com>
2020-06-22 00:09:40 +0900

    add: add AppBar Widge

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,9 @@ class MyApp extends StatelessWidget {
   // overrideアノテーションは無くても正常に動作するが、上書きしていることを明示的に示すために記載
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello'));
+    return MaterialApp(home: Scaffold(
+      appBar: AppBar(title: Text('MY FIRST APP'),),
+      body: Text('This is my default text!'),
+    ));
   }
 }
