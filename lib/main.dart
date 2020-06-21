3e3bc0d16eeac7bbf837d18462e1724469f481a8
mutohiroshi <mutohiroshi@team-lab.com>
2020-06-21 23:21:19 +0900

    add: add override annotation

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // overrideアノテーションは無くても正常に動作するが、上書きしていることを明示的に示すために記載
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(home: Text('Hello'));
   }
